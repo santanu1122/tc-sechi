82d3bda44997b09ba4163e8dec13db8bb719d898
Shawn Du <shawn.du@grandviewapps.com>
2014-06-13 21:16:46 -0400

    finish product table cell

---
+++ sechi/Views/SEProductTableViewCell.swift
@@ -0,0 +1,9 @@
+/**
+ *  Custom table view cell for product list view
+ */
+class SEProductTableViewCell: SESwipeableTableViewCell {
+
+	@IBOutlet var productName: UILabel
+	@IBOutlet var productCode: UILabel
+
+}
